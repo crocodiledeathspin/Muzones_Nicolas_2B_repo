b7fd3b7b417e11552b9ef58d7d8bec3aa4ad0e98
crocodiledeathspin <nicklashcargando@gmail.com>
2025-03-20 17:48:16 +0800

    Battleship

--- Battleship/battleship_v2.js
+++ Battleship/battleship_v2.js
@@ -1,43 +1,48 @@
-var randomLoc = Math.floor(Math.random() * 5);
+    
+    
+    var randomLoc = Math.floor(Math.random() * 5);
 
-var location1 = randomLoc;
-var location2 = location1 + 1;
-var location3 = location2 + 1;
+    var location1 = randomLoc;
+    var location2 = location1 + 1;
+    var location3 = location2 + 1;
 
-var guess;
-var hits = 0;
-var guesses = 0;
+    let guess;
+    let hits = 0; 
+    let guesses = 0;
 
-var isSunk = false;
+    let isSunk = false;
 
+    // Game Logic
 
-while (isSunk == false) {
-    guess = prompt("Ready!, Aim!, and Fire! (Enter number 0-6):");
+    while (!isSunk) {
+        guess = prompt("Ready!, Aim!, Fire! (Enter number 0-6)");
 
-    if (guess == null ) {
-        alert("Thank you for playing");
-        break;
-    }
+        if (guess == null) {
+            alert("Thank you for playing!");
+            break;
+        }
 
-    if (guess < 0 || guess > 6) {
-        alert("Please enter a valid cell number! ^-^")
-    }else {
-        guesses = guesses + 1;
-
-        if( guess == location1 || guess == location2 || guess == location3) {
-            hits = hits + 1;
-            alert("Hit!")
-
-            if (hits == 3) {
-                isSunk = true;
-                alert("You sank my battleship!")
-            }
+        if (guess < 0 || guess > 6) {
+            alert("Please enter a valid cell number!")
         }else {
-            alert("Miss!")
+            guesses += 1;
+
+            if (guess == location1 || guess == location2 || guess == location3) {
+                hits += 1;
+                alert("Hit!");
+
+                if (hits == 3) {
+                    isSunk = true;
+                    alert("You sunk my battleship!")
+                }
+            }else {
+                alert("Miss!");
+            }
         }
     }
-}
- 
-var stats = "You sank my battleship at " + guesses + " guesses " + "and your accuracy is" |+ (3/guesses);
 
-alert(stats);
+    var accuracy = (guesses > 0 ? (hits / guesses) * 100 : 0).toFixed(2);
+
+    var stats = "You took " + guesses + " guesses to sink the battleship!" + "\n" + "Accuracy: " + accuracy + "%";
+    
+    alert(stats);
--- Getting Function/function.js
+++
@@ -1,7 +0,0 @@
-var dogName = "rover";
-var dogWeight = 23;
-if (dogWeight > 20) {
-    console.log(dogName + "says WOOF WOOF");
-} else {
-    console.log(dogName + "says WOOF WOOF");
-}
